2667609a80adc031fa35caa842289ef10d12f227
umaumax <umauma7083@gmail.com>
2019-01-10 21:41:21 +0900

    Fix CXX.sh bug

--- CXX.sh
+++ CXX.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
-function main() {
-	local cmd="$CXX $*"
-	local output_filepath=$(echo "$cmd" | grep -E -o ' *\-o *[~/a-zA-Z0-9\-_.]*' | sed 's/ *-o *//g')
-	local asmcmd=$(echo "$cmd" | sed -E 's: *-o *[~/a-zA-Z0-9\-_.]*::')" -S -o $output_filepath.asm"
-	local output_filename=$(basename $output_filepath)
-	echo "echo '$output_filename:'; pushd $PWD >/dev/null 2>&1; $asmcmd; popd >/dev/null 2>&1"
-}
+
 OUTPUT_WD=${1:-$PWD}
 shift 1
 CXX=${1:-c++}
 shift 1
-main "$@" | tee -a $OUTPUT_WD/make.asm.sh
+
+function main() {
+	local cmd="$CXX $*"
+	local output_filepath=$(echo "$cmd" | grep -E -o ' *\-o *[~/a-zA-Z0-9\-_.]*' | sed 's/ *-o *//g')
+	[[ -z $output_filepath ]] && return # NOTE: no -o option
+	local asmcmd=$(echo "$cmd" | sed -E 's: *-o *[~/a-zA-Z0-9\-_.]*::')" -S -o $output_filepath.asm"
+	local output_filename=$(basename $output_filepath)
+	echo "echo '$output_filename:'; pushd $PWD >/dev/null 2>&1; $asmcmd; popd >/dev/null 2>&1" | tee -a $OUTPUT_WD/make.asm.sh
+	eval "$cmd"
+	return
+}
+
+main "$@"
+exit $?
